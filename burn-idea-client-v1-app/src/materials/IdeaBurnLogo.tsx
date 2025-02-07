9f8a6e18606ff36c31b9dd1e0bd200b58fcae905
Aidan Wigmore <aidanwigmore4@gmail.com>
2025-02-06 20:44:41 -0500

    Fix Admin Title in Logo

--- burn-idea-client-v1-app/src/materials/IdeaBurnLogo.tsx
+++ burn-idea-client-v1-app/src/materials/IdeaBurnLogo.tsx
@@ -17,16 +17,28 @@ function IdeaBurnLogo({ admin }: IdeaBurnLogoProps) {
             sx={{display: 'inline-flex', alignItems: 'center'}}
         >
             <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="IdeaBurn Logo" style={{width: '50px', height: '50px'}} />            
-            <Text 
-                size={Size.large}
-                text={
-                    admin && admin===true ? (
-                        'IDEABURN - ADMIN'
-                    ) : (
-                        'IDEABURN'
-                    )
-                }
-            />
+                <Box
+                    sx={{
+                        display: 'inline-flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Box 
+                        sx={{
+                            textAlign: 'right',
+                            transform: 'translateY(15px)',
+                        }}
+                    >
+                        {
+                            admin && admin===true && (
+                                <Text size={Size.small} text={'ADMIN'}/>
+                            )
+                        }
+                    </Box>
+                    <Box>
+                        <Text size={Size.large} text={'IDEABURN'}/>                
+                    </Box>
+                </Box>
         </Box>
     );
 };
