b72a46735ad45499a978d07e4817ebd5984a09f7
Aidan Wigmore <aidanwigmore4@gmail.com>
2025-02-06 21:50:46 -0500

    Re-routing Admin Header, Admin 'Customer View' Button

--- burn-idea-client-v1-app/src/header/Header.tsx
+++ burn-idea-client-v1-app/src/header/Header.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-
+ 
 import { useAuth } from '@context/AuthContext';
 
+import Tooltip from '@mui/material/Tooltip';
+import NoAccountsIcon from '@mui/icons-material/NoAccounts';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
@@ -24,6 +25,15 @@ function Header({ modalOverLayOpen, setModalOverLayOpen, admin }: HeaderProps) {
   const { customer } = useAuth();
 
   const handleClick = () => {
+    if (admin && admin === true) {
+      navigate(`/admin`);
+    } else {
+      navigate(`/`);
+    }
+  };
+
+  const handleViewerClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
     navigate(`/`);
   };
 
@@ -55,9 +65,19 @@ function Header({ modalOverLayOpen, setModalOverLayOpen, admin }: HeaderProps) {
         <Box sx={{display: 'inline-flex', flexDirection: 'row', justifyContent: 'flex-start', width: '99%' }}>
           <IdeaBurnLogo admin={admin}/>
         </Box>
-        <Box sx={{display: 'inline-flex', flexDirection: 'row', justifyContent: 'flex-end', width: '99%', paddingRight: '12px' }}>
+        <Box sx={{display: 'inline-flex', flexDirection: 'row', justifyContent: 'flex-end', width: '99%', gap: '0'}}>
           { customer && customer.givenName &&
-            <Text text={`Welcome, ${customer.givenName}.`} size={Size.small}/>
+            <>
+              {
+                admin && admin === true &&
+                <Tooltip title="Navigate to Viewer page?" arrow>
+                  <Button sx={{ color: 'white', fontSize: '12px', display: 'flex', flexDirection: 'column', paddingBottom: '10px' }} onClick={handleViewerClick}>
+                    <NoAccountsIcon sx={{ width: '36px', height: '36px' }} />
+                  </Button>
+                </Tooltip>
+              }
+              <Text text={`Welcome, ${customer.givenName}.`} size={Size.small}/>
+            </>
           }
         </Box>
       </Button>
